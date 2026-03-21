85170a0823f0ea9ae5855dfa57d467ee9db5b588
mohamed elsherif <mohamed.hatem97825@gmail.com>
2026-03-21 22:21:46 +0200

    dfjslf

--- 01-classes-objects/src/Main.java
+++ 01-classes-objects/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Student student1 = new Student();
 
-
+//comment
        student1.name = "mohamed";
        student1.age = 18;
        student1.grade = 75;
