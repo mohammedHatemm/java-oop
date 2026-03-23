2a0dc2114cf9f7e63178b7ff36339abdaf91d489
mohamed elsherif <mohamed.hatem97825@gmail.com>
2026-03-23 14:00:05 +0200

    the validation of the supper class

--- 04-inheritance/school/Person.java
+++ 04-inheritance/school/Person.java
@@ -1,2 +1,13 @@
 public class Person {
+
+    protected String name;
+    protected int age;
+
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+    public void displayInfo(){}
+    public void  introduce(){}
 }
