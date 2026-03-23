45cd34384511bf7c3caa1c96de5febb80cda402e
mohamed elsherif <mohamed.hatem97825@gmail.com>
2026-03-23 14:10:36 +0200

    update the person class

--- 04-inheritance/school/Person.java
+++ 04-inheritance/school/Person.java
@@ -8,6 +8,11 @@ public class Person {
         this.name = name;
         this.age = age;
     }
-    public void displayInfo(){}
-    public void  introduce(){}
+    public void displayInfo(){
+        System.out.println("name is :" + this.name);
+        System.out.println("age is :" + this.age);
+    }
+    public void  introduce(){
+        System.out.println("I am a " + this.name);
+    }
 }
