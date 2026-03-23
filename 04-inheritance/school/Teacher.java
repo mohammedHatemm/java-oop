56c6093b7b30a6e75f85535cd97bf40a596e574e
mohamed elsherif <mohamed.hatem97825@gmail.com>
2026-03-23 14:11:12 +0200

    update the teacher class

--- 04-inheritance/school/Teacher.java
+++ 04-inheritance/school/Teacher.java
@@ -1,2 +1,24 @@
-public class Teacher {
+public class Teacher extends Person{
+
+    private String employeeId;
+    private String subjct;
+
+    public Teacher( String name,int age , String employeeId, String subject){
+        super(name,age);
+        this.employeeId = employeeId;
+        this.subjct = subject;
+
+    }
+    @Override
+    public void displayInfo(){
+        super.displayInfo();
+        System.out.println("the employee id is " + this.employeeId);
+        System.out.println("the subjct is " + this.subjct);
+
+    }
+    @Override
+    public void introduce(){
+        System.out.println("I am a teacher and this is my name  " + this.name);
+    }
+
 }
