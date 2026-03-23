19fae780ba25addbd9904887a3ed43dc675745ad
mohamed elsherif <mohamed.hatem97825@gmail.com>
2026-03-23 14:17:49 +0200

    student class with imolentaion

--- 04-inheritance/school/Student.java
+++ 04-inheritance/school/Student.java
@@ -1,3 +1,17 @@
 public class Student  extends Person{
+ private String studentId;
+ public Student(String name, int age , String studentId){
+     super(name , age);
+     this.studentId = studentId;
+ }
+ @Override
+    public void introduce(){
+     System.out.println("I am a student and this is my name  " + this.name);
+ }
+ @Override
+    public void displayInfo(){
+     super.displayInfo();
+     System.out.println("Student id is " + this.studentId);
 
+    }
 }
