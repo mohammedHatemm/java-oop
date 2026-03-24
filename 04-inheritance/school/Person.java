3db11f5d0c44f31cf482d0d90e7fb7558dff71cc
mohamed elsherif <mohamed.hatem97825@gmail.com>
2026-03-24 09:56:44 +0200

    Implement Person abstract class with name, age, id fields and abstract methods

--- 04-inheritance/school/Person.java
+++ 04-inheritance/school/Person.java
@@ -1,18 +1,66 @@
-public class Person {
+import java.util.Objects;
+
+public abstract class Person {
 
     protected String name;
     protected int age;
+    protected String id;
 
-
-    public Person(String name, int age) {
+    public Person(String name, int age, String id) {
         this.name = name;
         this.age = age;
+        this.id = id;
     }
-    public void displayInfo(){
-        System.out.println("name is :" + this.name);
-        System.out.println("age is :" + this.age);
+
+    public abstract void displayInfo();
+    public abstract void introduce();
+
+    public String getName() {
+        return name;
     }
-    public void  introduce(){
-        System.out.println("I am a " + this.name);
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id='" + id + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+
+        Person person = (Person) obj;
+        return age == person.age &&
+                Objects.equals(name, person.name) &&
+                Objects.equals(id, person.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age, id);
     }
 }
