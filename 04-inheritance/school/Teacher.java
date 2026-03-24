ee17509c859c3867f59d8e3e1f230b84435f1da0
mohamed elsherif <mohamed.hatem97825@gmail.com>
2026-03-24 09:57:10 +0200

    Implement Teacher class extending Person with subject and salary

--- 04-inheritance/school/Teacher.java
+++ 04-inheritance/school/Teacher.java
@@ -1,24 +1,71 @@
-public class Teacher extends Person{
+import java.util.Objects;
 
-    private String employeeId;
-    private String subjct;
+public class Teacher extends Person {
 
-    public Teacher( String name,int age , String employeeId, String subject){
-        super(name,age);
-        this.employeeId = employeeId;
-        this.subjct = subject;
+    protected String subject;
+    protected double salary;
 
+    public Teacher(String name, int age, String id, String subject, double salary) {
+        super(name, age, id);
+        this.subject = subject;
+        this.salary = salary;
     }
+
     @Override
-    public void displayInfo(){
-        super.displayInfo();
-        System.out.println("the employee id is " + this.employeeId);
-        System.out.println("the subjct is " + this.subjct);
-
+    public void displayInfo() {
+        System.out.println("=== Teacher Info ===");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("ID: " + id);
+        System.out.println("Subject: " + subject);
+        System.out.println("Salary: $" + salary);
     }
+
     @Override
-    public void introduce(){
-        System.out.println("I am a teacher and this is my name  " + this.name);
+    public void introduce() {
+        System.out.println("Hello, I'm " + name + ", I teach " + subject);
     }
 
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Teacher{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id='" + id + '\'' +
+                ", subject='" + subject + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        if (!super.equals(obj)) return false;
+
+        Teacher teacher = (Teacher) obj;
+        return Double.compare(teacher.salary, salary) == 0 &&
+                Objects.equals(subject, teacher.subject);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), subject, salary);
+    }
 }
