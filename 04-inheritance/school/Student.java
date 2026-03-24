385b976830ce7623019edb6897fea70018fbaacc
mohamed elsherif <mohamed.hatem97825@gmail.com>
2026-03-24 09:57:24 +0200

    Implement Student class extending Person with major and GPA

--- 04-inheritance/school/Student.java
+++ 04-inheritance/school/Student.java
@@ -1,17 +1,71 @@
-public class Student  extends Person{
- private String studentId;
- public Student(String name, int age , String studentId){
-     super(name , age);
-     this.studentId = studentId;
- }
- @Override
-    public void introduce(){
-     System.out.println("I am a student and this is my name  " + this.name);
- }
- @Override
-    public void displayInfo(){
-     super.displayInfo();
-     System.out.println("Student id is " + this.studentId);
+import java.util.Objects;
 
+public class Student extends Person {
+
+    protected String major;
+    protected double gpa;
+
+    public Student(String name, int age, String id, String major, double gpa) {
+        super(name, age, id);
+        this.major = major;
+        this.gpa = gpa;
+    }
+
+    @Override
+    public void displayInfo() {
+        System.out.println("=== Student Info ===");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("ID: " + id);
+        System.out.println("Major: " + major);
+        System.out.println("GPA: " + gpa);
+    }
+
+    @Override
+    public void introduce() {
+        System.out.println("Hi, I'm " + name + ", a student majoring in " + major);
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", id='" + id + '\'' +
+                ", major='" + major + '\'' +
+                ", gpa=" + gpa +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        if (!super.equals(obj)) return false;
+
+        Student student = (Student) obj;
+        return Double.compare(student.gpa, gpa) == 0 &&
+                Objects.equals(major, student.major);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(super.hashCode(), major, gpa);
     }
 }
